38695d02688792e6c267ba8f5658f631ae521b34
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-05 01:16:33 -0300

    Structure optimized

--- db/migrate/20140601003917_create_projects.rb
+++ db/migrate/20140601003917_create_projects.rb
@@ -1,10 +1,10 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-      t.string :name
-      t.string :description
-      t.string :url_video
-      t.belongs_to :user, index: true
+      t.string :name, limit: 50, null: false
+      t.string :resume, limit: 144, null: false
+      t.string :url_video, limit: 50, null: true
+      t.references :creater, index: true
 
       t.timestamps
     end

260e399dce84e20feb81944b0d92856638702488
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-05 00:39:51 -0300

    Email academico added

--- db/migrate/20140601001619_create_users.rb
+++ db/migrate/20140601001619_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name, limit: 55, null: false
       t.string :email, limit: 55, unique: true, null: false
+      t.string :academic_email, limit: 55, unique: true, null: true
       t.string :pass_salt, limit: 29, null: false
       t.string :password, limit: 60, null: false
 

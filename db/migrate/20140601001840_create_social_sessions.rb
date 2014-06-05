fa98f80613bc3f6b7685c12ce788869e168ef6f0
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-05 00:38:00 -0300

    Structure UPDATED & Role, useless deleted

--- db/migrate/20140601001840_create_social_sessions.rb
+++ db/migrate/20140601001840_create_social_sessions.rb
@@ -1,7 +1,10 @@
 class CreateSocialSessions < ActiveRecord::Migration
   def change
     create_table :social_sessions do |t|
-      t.integer :id_on_social
+      t.string :id_on_social, limit: 45, null: false
+      t.string :name, limit: 65, null: false
+      t.string :username, limit: 45, null: false
+      t.string :email, limit: 55, null: false
       t.belongs_to :user
       t.belongs_to :social_network
 

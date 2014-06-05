b780ad4562485f838719587ec11f53918dc74008
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-05 01:20:08 -0300

    Base Structure for the DescriptionItem

---
+++ db/migrate/20140605041715_create_description_items.rb
@@ -0,0 +1,11 @@
+class CreateDescriptionItems < ActiveRecord::Migration
+  def change
+    create_table :description_items do |t|
+      t.string :title, limit: 75, null: false
+      t.string :long_description, limit: 4294967295, null: false
+      t.belongs_to :project, index: true
+
+      t.timestamps
+    end
+  end
+end

7cefd04aececb222aefa4ba6103826650fe38ec8
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-05 01:30:42 -0300

    User relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  has_one :social_session
+  has_many :transactions
+  has_many :projects
+
   validates_confirmation_of :password
   validates_confirmation_of :email
 end

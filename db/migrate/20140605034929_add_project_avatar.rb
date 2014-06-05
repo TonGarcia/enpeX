ac34458d984af0227bbb615f981e853a49750825
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-05 00:51:23 -0300

    Project avatar added

---
+++ db/migrate/20140605034929_add_project_avatar.rb
@@ -0,0 +1,15 @@
+class AddProjectAvatar < ActiveRecord::Migration
+  def self.up
+    add_column :projects, :avatar_piece_file_name, :string, :after => :user_id
+    add_column :projects, :avatar_piece_content_type, :string, :after => :avatar_piece_file_name
+    add_column :projects, :avatar_piece_file_size, :integer, :after => :avatar_piece_content_type
+    add_column :projects, :avatar_piece_updated_at, :datetime, :after => :avatar_piece_file_size
+  end
+
+  def self.down
+    remove_column :projects, :avatar_piece_file_name
+    remove_column :projects, :avatar_piece_content_type
+    remove_column :projects, :avatar_piece_file_size
+    remove_column :projects, :avatar_piece_updated_at
+  end
+end

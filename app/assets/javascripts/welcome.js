526817bb19c96623241b07bb51411de18751488c
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-05 23:49:00 -0300

    homeController added

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,3 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
+
+enpeXApp.controller('HomeController', function ($scope, $http) {
+    // The HomeController (control all the home/landing)
+    $scope.progressed_sample = "0%";
+    $scope.progressed_sample = "80%";
+});

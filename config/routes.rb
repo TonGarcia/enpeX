d269ce4ab7e85ef07095e4f56fc6f72e6a3576f0
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-06 16:47:14 -0300

    routes list refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root :to => 'welcome#index'
+  root :to => 'welcome#home'
+  get 'users/sign_out' => 'users#sign_out'
+
   resources :description_items
   resources :transactions
   resources :projects
   resources :users
-
-  get 'users/sign_out' => 'users#sign_out'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

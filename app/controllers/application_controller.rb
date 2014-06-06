a5c5365b4e0ae1fa0b1082bf7bd430bc783c7c1f
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-06 17:04:09 -0300

    redirect to home if there is no session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # SetUp user
+  before_action :setup_user, :except => [:home, 'users#create']
+
+  def setup_user
+    # SetUp the user to prevent finds on BD
+    if session['user_id'].nil? && params['action'] != 'sign_out'
+      redirect_to root_path
+      return
+    end
+    if @user.nil? then @user = User.find(session['user_id']) end
+  end
 end

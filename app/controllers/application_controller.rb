83a809d0d713559ad16416a4fceb21237dfb2938
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-06 17:31:21 -0300

    Delete the session if the session exist on memory but not on the DB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # SetUp user
-  before_action :setup_user, :except => [:home, 'users#create']
+  before_action :setup_user
 
   def setup_user
     # SetUp the user to prevent finds on BD
-    if session['user_id'].nil? && params['action'] != 'sign_out'
-      redirect_to root_path
-      return
+    if session['user_id'].nil?
+      if params['action'] != 'sign_out' && params['action'] != 'home'
+        if params['controller'] != 'users' && params['action'] != 'create'
+          redirect_to root_path
+          return
+        end
+      end
     end
-    if @user.nil? then @user = User.find(session['user_id']) end
+
+    if @user.nil? && !session['user_id'].nil?
+      @user = User.where(id: session['user_id']).take!
+      session.delete('user_id') if @user.nil?
+    end
   end
 end

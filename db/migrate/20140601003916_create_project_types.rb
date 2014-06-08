02ccd5c9d02e5b688ec2c7b8c02f28162d0a8178
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-08 14:56:22 -0300

    description limit edited

--- db/migrate/20140601003916_create_project_types.rb
+++ db/migrate/20140601003916_create_project_types.rb
@@ -2,7 +2,7 @@ class CreateProjectTypes < ActiveRecord::Migration
   def change
     create_table :project_types do |t|
       t.string :name, limit: 30, null: false
-      t.string :description, limit: 30, null: true
+      t.string :description, limit: 140, null: true
 
       t.timestamps
     end

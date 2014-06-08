4cb37adebe52c307a558e140c248e7195aff1043
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-08 08:13:08 -0300

    - Active attr added; - Active alias_name added;

--- db/migrate/20140601003917_create_projects.rb
+++ db/migrate/20140601003917_create_projects.rb
@@ -2,8 +2,10 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.string :name, limit: 50, null: false
+      t.string :alias_name, limit: 50, null: false
       t.string :resume, limit: 144, null: false
       t.string :url_video, limit: 50, null: true
+      t.boolean :active, default: true, null: false
       t.references :creator, index: true
 
       t.timestamps

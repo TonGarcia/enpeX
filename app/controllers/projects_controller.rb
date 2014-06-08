0ebf367d6d81d06e3d33be6a799bc4fc46d642da
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-08 08:12:01 -0300

    - Added to skip the login required for show & index of projects created.
    - Active attr added.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  skip_before_filter :setup_user, only: [:show, :index]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   # GET /projects
@@ -70,6 +71,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :resume, :url_video)
+      params.require(:project).permit(:name, :resume, :url_video, :active)
     end
 end

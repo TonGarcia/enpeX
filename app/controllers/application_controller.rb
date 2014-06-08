9e5dc79a10ea270d8e7bad93644da4c599e01728
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-08 08:32:46 -0300

    removed the code skip (moved it to the respective controller). added the "permission" to the projects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,23 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # SetUp user
-  before_action :setup_user
-  before_filter :setup_user
+  before_action :setup_user, except: :sign_out
+  before_filter :setup_user, except: :sign_out
 
   def setup_user
-    # SetUp the user to prevent finds on BD
-    if session['user_id'].nil?
-      if params['action'] != 'sign_out' && params['action'] != 'home'
-        if (params['controller'] != 'users' && params['action'] != 'create') || (params['controller'] != 'users' && params['action'] != 'login')
-          redirect_to root_path
-          return
-        end
-      end
-    end
-
-    if @current_user.nil? && !session['user_id'].nil?
+    unless session['user_id'].nil?
       @current_user = User.where(id: session['user_id']).take!
       session.delete('user_id') if @current_user.nil?
+    end
+
+    # SetUp the user to prevent finds on BD
+    if session['user_id'].nil?
+      if (params['controller'] != 'projects' && params['action'] != 'show')
+        redirect_to root_path
+        return
+      end
     end
   end
 end

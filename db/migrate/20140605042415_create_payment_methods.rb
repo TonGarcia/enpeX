7ca7f5e88f5fd7a296bd3ee745fa127d5e9949fb
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-08 15:15:23 -0300

    through added (say more, like how this payment came)

--- db/migrate/20140605042415_create_payment_methods.rb
+++ db/migrate/20140605042415_create_payment_methods.rb
@@ -1,6 +1,7 @@
 class CreatePaymentMethods < ActiveRecord::Migration
   def change
     create_table :payment_methods do |t|
+      t.string :through, limit: 55, null: false
       t.string :type, limit: 55, null: false
       t.string :description, limit: 140, null: true
 

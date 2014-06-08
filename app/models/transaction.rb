5bf3db7b006c7f084743c765f77e9a687442bf89
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-08 14:58:07 -0300

    currency association added to the transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,7 @@ class Transaction < ActiveRecord::Base
   belongs_to :user, :class_name => 'User', foreign_key: :payer_id
   belongs_to :project, :class_name => 'Project', foreign_key: :receiver_id
   belongs_to :payment_method
+  belongs_to :currency
   has_one :receipt
 
   # Attrs validations

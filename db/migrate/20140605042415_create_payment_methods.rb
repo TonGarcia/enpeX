bf66dd20a93b00445358e776397656119ff5c0da
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-08 14:34:23 -0300

    method_type cant be null

--- db/migrate/20140605042415_create_payment_methods.rb
+++ db/migrate/20140605042415_create_payment_methods.rb
@@ -2,7 +2,7 @@ class CreatePaymentMethods < ActiveRecord::Migration
   def change
     create_table :payment_methods do |t|
       t.string :name, limit: 50, null: false
-      t.string :method_type, limit: 50, null: true
+      t.string :method_type, limit: 50, null: false
       t.string :description, limit: 140, null: true
 
       t.timestamps

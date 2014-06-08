e787ca064c1bb3ddf91f97f11fad35a5cb13fba3
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-08 14:36:24 -0300

    - removed currency as attr; - NOT NULL to associate attrs.

--- db/migrate/20140601005026_create_transactions.rb
+++ db/migrate/20140601005026_create_transactions.rb
@@ -2,11 +2,11 @@ class CreateTransactions < ActiveRecord::Migration
   def change
     create_table :transactions do |t|
       t.decimal :value, :decimal, precision: 9, scale: 2, null: false
-      t.string :currency, limit: 30, null: false
       t.boolean :banking, null: false
-      t.belongs_to :payment_method, index: true
-      t.references :payer, index: true
-      t.references :receiver, index: true
+      t.references :payer, index: true, null: false
+      t.references :receiver, index: true, null: false
+      t.belongs_to :payment_method, index: true, null: false
+      t.belongs_to :currency, index: true, null: false
 
       t.timestamps
     end

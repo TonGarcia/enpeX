f9d44414d31c306d6967d018421b9d2a74e03bf2
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-08 08:29:45 -0300

    alias_name added to project params required

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,4 @@
 class ProjectsController < ApplicationController
-  skip_before_filter :setup_user, only: [:show, :index]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   # GET /projects
@@ -71,6 +70,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :resume, :url_video, :active)
+      params.require(:project).permit(:name, :alias_name, :resume, :url_video, :active)
     end
 end

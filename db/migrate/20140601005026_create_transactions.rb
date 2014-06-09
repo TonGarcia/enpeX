d079c4c8c3ffd4828502c8c0ae66af8d5fea4744
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-09 10:17:40 -0300

    created the reference to the external payment gateway (receipt_access)

--- db/migrate/20140601005026_create_transactions.rb
+++ db/migrate/20140601005026_create_transactions.rb
@@ -3,9 +3,9 @@ class CreateTransactions < ActiveRecord::Migration
     create_table :transactions do |t|
       t.decimal :value, :decimal, precision: 9, scale: 2, null: false
       t.boolean :banking, null: false
+      t.string :receipt_access, limit: 140, null: false
       t.references :payer, index: true, null: false
       t.references :receiver, index: true, null: false
-      t.belongs_to :payment_method, index: true, null: false
       t.belongs_to :currency, index: true, null: false
 
       t.timestamps
